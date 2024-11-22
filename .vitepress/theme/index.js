448e253173a734695635065a885632b98a5d41a0
perfree <perfree@126.com>
2024-11-22 16:26:46 +0800

    add comment

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,10 +2,12 @@ import DefaultTheme from 'vitepress/theme'
 import { onMounted, watch, nextTick } from 'vue'
 import { useRoute } from 'vitepress'
 import mediumZoom from 'medium-zoom'
+import Layout from "./Layout.vue";
 
 import './index.css'
 export default {
     ...DefaultTheme,
+    Layout: Layout,
     setup() {
         const route = useRoute()
         const initZoom = () => {
